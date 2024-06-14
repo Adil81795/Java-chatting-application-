2487c665b084e42f205d060622b80e0b16f316e6
Adil Zafar <adilzafar81795@.com>
2024-06-14 20:36:22 +0530

    commit 8

---
+++ src/client.java
@@ -0,0 +1,43 @@
+package chatting.application;
+
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.text.*;
+import java.net.*;
+import java.io.*;
+
+public class Client implements ActionListener {
+
+    JTextField text;
+    static JPanel a1;
+    static Box vertical = Box.createVerticalBox();
+
+    static JFrame f = new JFrame();
+
+    static DataOutputStream dout;
+
+    Client() {
+
+        f.setLayout(null);
+
+        JPanel p1 = new JPanel();
+        p1.setBackground(new Color(7, 94, 84));
+        p1.setBounds(0, 0, 450, 70);
+        p1.setLayout(null);
+        f.add(p1);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/3.png"));
+        Image i2 = i1.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel back = new JLabel(i3);
+        back.setBounds(5, 20, 25, 25);
+        p1.add(back);
+
+        back.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent ae) {
+                System.exit(0);
+            }
+        });
